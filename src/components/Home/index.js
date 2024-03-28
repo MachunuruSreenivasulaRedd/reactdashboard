c1de074cd9d9b0d731d0f019d42e13f35a6e0813
MachunuruSreenivasulaRedd <83321517+MachunuruSreenivasulaRedd@users.noreply.github.com>
2024-03-28 15:40:45 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,50 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom';
-import { Navigate } from 'react-router-dom'
-export default function Home() {
-  const location = useLocation();
+import React, { useEffect,useState } from 'react'
+import './index.css'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { CryptoPrices } from '../CryptoPrices';
+export const Home = props => {
+  const {visible} = props
+  const [data,setData] = useState([])
+  useEffect( ()=> {
+    getDetail()
+  },[])
+  const getDetail = async() => {
+    const response = await fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population')
+    const details=await response.json()
+    const {data}=details
+    const updatedData = data.map(item => ({
+      Population: item.Population/100000,
+      Year:item.Year
+    }))
+    console.log(data)
+    setData(updatedData)
+   
+  }
+
   return (
-    <Navigate to="/login" state={{from:location}}/>
+    <div className={visible ? 'homeContainer': 'sideHomeContainer'}>
+      <div className='chart'>
+        <LineChart
+          width={300}
+          height={250}
+          data={data}
+          margin={{
+            top: 5,
+            right: 25,
+            left: 0,
+            bottom: 5,
+          }}
+        >
+          <XAxis  dataKey="Year" label={{value:'Year', position: "insideBottomCenter", dy: 15}}/>
+          <YAxis dataKey="Population" label={{value:'Population',position: "insideLeft", angle: -90,   dy: 20}}/>
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="Population" stroke="green" strokeWidth={4}/>
+        </LineChart>
+      </div>
+      <div className='prices'>
+        <CryptoPrices />
+      </div>
+    </div>
   )
 }
